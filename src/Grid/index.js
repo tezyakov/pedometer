ff95a2ef4cc79831d3d4776061be92685960a9fd
Тезяков Никита Андреевич <natezyakov@edu.hse.ru>
2020-02-29 23:42:07 +0500

    classes into functional components

--- src/Grid/index.js
+++ src/Grid/index.js
@@ -1,75 +1,72 @@
 import React, { Component } from 'react';
 import { AgGridReact } from 'ag-grid-react';
 import axios from 'axios';
+import { Provider, connect } from 'react-redux';
+
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
-
 import styles from './styles.module.scss';
 import '../ag-grid-overrides.scss'
 
 
-class Gridd extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      columnDefs: [{
-        headerName: "Дата", 
-        field: "date", 
-        editable: true, 
-        width: 158, 
+const Gridd = () => {
+  const [values, setValues] = React.useState({
+    columnDefs: [{
+      headerName: "Дата", 
+      field: "date", 
+      editable: true, 
+      width: 158, 
 
-      }, {
-        headerName: "Дистанция", 
-        field: "distance", 
-        editable: true, 
-        width: 175,
-      }],
-      rowData: [{
-        date: "Toyota", 
-        distance: "Celica"
-      }, {
-        date: "Toyota", 
-        distance: "Celica"
-      }, {
-        date: "Toyota", 
-        distance: "Celica"
-      }, {
-        date: "Toyota", 
-        distance: "Celica"
-      }, {
-        date: "Toyota", 
-        distance: "Celica"
-      }]
-      
-    }
-  };
+    }, {
+      headerName: "Дистанция", 
+      field: "distance", 
+      editable: true, 
+      width: 175,
+    }],
+    rowData: [{
+      date: "Toyota", 
+      distance: "Celica"
+    }, {
+      date: "Toyota", 
+      distance: "Celica"
+    }, {
+      date: "Toyota", 
+      distance: "Celica"
+    }, {
+      date: "Toyota", 
+      distance: "Celica"
+    }, {
+      date: "Toyota", 
+      distance: "Celica"
+    }]
+  })
 
-  componentDidMount() {
-    axios.get('http://localhost:3000/walking')
-    .then(data => data.data)
-    .then(rowData => this.setState({rowData}))
+  const getData = async() => {
+    const data = await axios.get('http://localhost:3000/walking');
+    const rowData = data.data;
+    setValues(rowData);
   }
 
-  render() {
-    return (
-      <div className={styles.contentContainer}>
-        <div
+  getData();
+  return (
+    <div className={styles.contentContainer}>
+      <div
         className="ag-theme-balham"
         style={{
         height: '500px',
         width: '335px' }}
       >
         <AgGridReact
-          rowHeight={40}
-          enableSorting={true}
-          columnDefs={this.state.columnDefs}
-          rowData={this.state.rowData}>
+        rowHeight={40}
+        enableSorting={true}
+        columnDefs={values.columnDefs}
+        rowData={values.rowData}>
         </AgGridReact>
-        <button className={styles.gridButton} onClick={() => alert('Не сделал')}>Добавить запись</button>
+      <button className={styles.gridButton} onClick={() => alert('Work in progress')}>Добавить запись</button>
       </div>
-      </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 
 export default Gridd;
