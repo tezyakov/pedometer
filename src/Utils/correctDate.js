bcab476df6c1cde064e46c0920f22006386b3c06
Тезяков Никита Андреевич <natezyakov@edu.hse.ru>
2020-03-01 16:40:22 +0500

    hotfix

--- src/Utils/correctDate.js
+++ src/Utils/correctDate.js
@@ -1,6 +1,6 @@
-const correctDate = (date) => {
+const setRightDate = (date) => {
   let data = date.slice(0,10).split('-');
   return data[2] + '.' + data[1] + '.' + data[0]
 }
 
-export default correctDate;
+export default setRightDate;
